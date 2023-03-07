c19853973c042756c2d2092553429e6b2caf274f
Mateusz Manart <mateusz.manart@itmagination.com>
2023-03-07 21:08:42 +0100

    files

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <div>
+        Test App Test AppTest AppTest AppTest AppTest AppTest AppTest AppTest AppTest AppTest AppTest AppTest AppTest App
+        </div>
+      </main>
+      <Footer />
+    </  div>
+
+  );
+}
+
+export default App;
